9ae27e6fbb3767e4b57be1436a1d64b559a11b42
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-09-14 21:50:45 +0800

    chore: add bundler

---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+// oxlint-disable typescript/no-unsafe-call
+
+import { defineConfig } from 'tsdown';
+import { StaleGuardRecorder } from 'tsdown-stale-guard';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  dts: true,
+  exports: true,
+  publint: true,
+  plugins: [StaleGuardRecorder()],
+});
