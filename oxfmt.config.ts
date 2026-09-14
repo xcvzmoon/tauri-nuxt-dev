181e9535b2606948ab2bf6e8cb3b26fb1501b9df
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-09-14 21:28:05 +0800

    chore: add formatter and linter

---
+++ oxfmt.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'oxfmt';
+
+export default defineConfig({
+  sortImports: {
+    groups: [
+      'type-import',
+      'type-internal',
+      ['type-parent', 'type-sibling', 'type-index'],
+      ['value-builtin', 'value-external'],
+      'value-internal',
+      ['value-parent', 'value-sibling', 'value-index'],
+      'unknown',
+    ],
+    newlinesBetween: false,
+  },
+  sortPackageJson: false,
+  singleQuote: true,
+});
